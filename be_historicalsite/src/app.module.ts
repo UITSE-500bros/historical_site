d45951423723d8ee501215debc15af8c7a80771f
Đoàn Phương Nam <120766900+DPhngNam@users.noreply.github.com>
2025-04-07 17:53:41 +0700

    Update app.module.ts

--- be_historicalsite/src/app.module.ts
+++ be_historicalsite/src/app.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { PaymentsModule } from './payments/payments.module';
+import { PaymentsModule } from './modules/payments/payments.module';
 
 @Module({
   imports: [PaymentsModule],
