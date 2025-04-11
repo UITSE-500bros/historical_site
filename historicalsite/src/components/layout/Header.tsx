87b31ecbe644db87c933fc57088def811fc088ba
ThnhHuynh04 <22521406@gm.uit.edu.vn>
2025-04-12 00:39:04 +0700

    feat: header and nav bar

--- historicalsite/src/components/layout/Header.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Header() {
-  return (
-    <div className='flex  items-center justify-between p-4 text-white bg-transparent h-[118px]'>
-      <div className='w-[78px] h-p100px text-2xl text-black'>Logo</div>
-    </div>
-  )
-}
