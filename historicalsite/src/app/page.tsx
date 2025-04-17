1c52a34003b3438da71c13e5d32da8c02b0653ce
ThnhHuynh04 <22521406@gm.uit.edu.vn>
2025-04-17 23:52:06 +0700

    feat: update banner and heading

--- historicalsite/src/app/page.tsx
+++ historicalsite/src/app/page.tsx
@@ -2,9 +2,39 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        Home
+    <div className="flex flex-col min-h-screen">
+      {/* Banner section */}
+      <main className="relative flex flex-col w-full">
+        {/* Banner Image */}
+        <div className="w-full">
+          <Image
+            src="/home/image 15.png"
+            alt="Home Banner"
+            layout="responsive"
+            className="w-full h-auto object-cover"
+            width={1440}
+            height={917}
+          />
+        </div>
+
+        {/* Overlay Text */}
+        <div className="absolute inset-0 flex flex-col justify-center items-center text-white">
+          {/* Heading 1 */}
+          <h1
+            className="font-[Actor] font-normal text-[76px] leading-[72px] tracking-[-0.05em] text-center uppercase"
+            style={{ lineHeight: "72px" }}
+          >
+            Lịch sử đương đại phương tây
+          </h1>
+
+          {/* Subheading */}
+          <p
+            className="mt-[60px]  mx-5 font-[Actor] font-normal text-[20px] leading-[28px] tracking-[0] text-center"
+            style={{ lineHeight: "28px" }}
+          >
+            Lịch sử phương Tây là hành trình đầy biến động của những đế chế hùng mạnh, những cuộc cách mạng tư tưởng và những thành tựu vĩ đại, từ nền dân chủ Athens cổ đại đến Cách mạng Công nghiệp, tất cả đã định hình thế giới hiện đại như ta biết ngày nay
+          </p>
+        </div>
       </main>
     </div>
   );
