8025e6c7a5a6359100b0a101c4844d28c9f992bf
ThnhHuynh04 <22521406@gm.uit.edu.vn>
2025-04-22 17:49:09 +0700

    feat: overview section

--- historicalsite/src/app/page.tsx
+++ historicalsite/src/app/page.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import { TitleSection } from "../components/section";
 
 export default function Home() {
   return (
@@ -32,8 +33,44 @@ export default function Home() {
             className="mt-[60px]  mx-5 font-[Actor] font-normal text-[20px] leading-[28px] tracking-[0] text-center"
             style={{ lineHeight: "28px" }}
           >
-            Lịch sử phương Tây là hành trình đầy biến động của những đế chế hùng mạnh, những cuộc cách mạng tư tưởng và những thành tựu vĩ đại, từ nền dân chủ Athens cổ đại đến Cách mạng Công nghiệp, tất cả đã định hình thế giới hiện đại như ta biết ngày nay
+            Lịch sử phương Tây là hành trình đầy biến động của những đế chế hùng
+            mạnh, những cuộc cách mạng tư tưởng và những thành tựu vĩ đại, từ
+            nền dân chủ Athens cổ đại đến Cách mạng Công nghiệp, tất cả đã định
+            hình thế giới hiện đại như ta biết ngày nay
           </p>
+          <button className="flex  h-14 justify-center items-center gap-1 [background:var(--Background,#FFFEF8)] px-12 py-5">
+            <a
+              href="/history"
+              className="text-[color:var(--Black,#1F1F1F)] text-center [leading-trim:both] [text-edge:cap] [font-family:Actor] text-base font-normal leading-[26px] uppercase"
+            >
+              Xem thêm
+            </a>
+          </button>
+        </div>
+
+        {/* Overview Section */}
+        <TitleSection title="Giới thiệu" />
+        <div className="flex justify-between items-end self-stretch">
+          <span className="w-full md:w-[400px] text-[color:var(--Black,#1F1F1F)] text-left [leading-trim:both] [text-edge:cap] [font-family:Actor] text-[20px] font-normal leading-[28px] tracking-[-0.05em]">
+            Trang web của chúng tôi là cánh cửa mở ra thế giới lịch sử phương
+            Tây, cung cấp những kiến thức sâu sắc, sự kiện quan trọng và bối
+            cảnh lịch sử đầy đủ. Từ những nền văn minh cổ đại đến các cuộc cách
+            mạng làm thay đổi thế giới, chúng tôi mang đến nội dung phong phú,
+            dễ hiểu và hấp dẫn, giúp bạn khám phá dòng chảy lịch sử phương Tây
+            một cách chân thực và sinh động.
+          </span>
+
+            <img
+              className="w-[400px] h-[300px] "
+              src="/home/monalisa.png"
+              alt="Mona Lisa"
+            />
+            <img
+              className="w-[400px] h-[600px] "
+              src="/home/pisa.png"
+              alt="Leaning Tower of Pisa"
+            />
+   
         </div>
       </main>
     </div>
---
+++ historicalsite/src/components/section/TitleSection.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+export default function TitleSection({ title }: { title: string }) {
+  return (
+    <div className=" flex justify-between items-center self-stretch h-[200px] ">
+      {/* Left Section */}
+      <div className=" flex justify-center items-center  w-[446px] h-[170px] shrink-0 border border-[color:var(--Black,#1F1F1F)] rounded-full border-solid">
+        <p className="text-[color:var(--Black,#1F1F1F)] text-center [leading-trim:both] [text-edge:cap] [font-family:Actor] text-[64px] font-normal leading-[44px] tracking-[-3.2px] uppercase">
+          {title}
+        </p>
+      </div>
+
+      {/* Right Section */}
+      <div className="flex justify-center items-center w-[200px] h-[50px] shrink-0  rounded-full bg-transparent cursor-pointer">
+        <a className="text-[color:var(--Black,#1F1F1F)] text-center [font-family:Actor] text-[20px] font-normal leading-[24px] tracking-[-1px] uppercase">
+          Tìm Hiểu Thêm
+        </a>
+      </div>
+    </div>
+  );
+}
