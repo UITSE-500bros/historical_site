829ff9194413b5500913bcf301586896d0532448
ThnhHuynh04 <22521406@gm.uit.edu.vn>
2025-04-25 23:01:29 +0700

    feat(explore): :adhesive_bandage: init tabs shadcn

--- historicalsite/src/app/explore/page.tsx
+++ historicalsite/src/app/explore/page.tsx
@@ -1,3 +1,4 @@
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@radix-ui/react-tabs";
 import React from "react";
 
 export default function Explore() {
@@ -11,6 +12,22 @@ export default function Explore() {
           Khám Phá
         </h1>
       </div>
+
+      <Tabs className="w-full mt-[80px] flex-col px-8 justify-center">
+        <TabsList className="flex gap-4 flex-wrap justify-center items-center">
+          <TabsTrigger value ="people" className="text-[color:var(--Black,#1F1F1F)] [leading-trim:both] [text-edge:cap] [font-family:Actor] text-base font-normal leading-[26px] uppercase"> Nhân vật tiêu biểu</TabsTrigger>
+          <TabsTrigger value ="event" className="text-[color:var(--Black,#1F1F1F)] [leading-trim:both] [text-edge:cap] [font-family:Actor] text-base font-normal leading-[26px] uppercase"> Sự kiện lịch sử</TabsTrigger>
+
+        </TabsList>
+        <TabsContent value="people">
+          a
+          </TabsContent>
+        <TabsContent value="event">
+          b
+          </TabsContent>
+      </Tabs>
+
+
     </div>
   );
 }
