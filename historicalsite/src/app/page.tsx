2ea16fb6509d66ae50673dbcb52cd06fe73b5fdf
ThnhHuynh04 <22521406@gm.uit.edu.vn>
2025-04-25 11:25:14 +0700

    style(explore): :art: httml5 improve doc

--- historicalsite/src/app/page.tsx
+++ historicalsite/src/app/page.tsx
@@ -9,8 +9,7 @@ export default function Home() {
     <div className="flex flex-col min-h-screen">
       {/* Banner section */}
       <main className="relative flex flex-col w-full">
-        {/* Banner Image + Overlay Container */}
-        <div className="relative w-full">
+        <header className="relative w-full">
           <Image
             src="/home/image 15.png"
             alt="Home Banner"
@@ -19,26 +18,19 @@ export default function Home() {
             width={1440}
             height={917}
           />
-
-          {/* Overlay Text inside Banner */}
           <div className="absolute inset-0 flex flex-col justify-center items-center text-white">
-            {/* Heading 1 */}
             <h1
               className="font-[Actor] font-normal text-[76px] leading-[72px] tracking-[-0.05em] text-center uppercase"
               style={{ lineHeight: "72px" }}
             >
               Lịch sử đương đại phương tây
             </h1>
-
-            {/* Subheading */}
             <p
               className="mt-[60px] mx-5 font-[Actor] font-normal text-[20px] leading-[28px] tracking-[0] text-center"
               style={{ lineHeight: "28px" }}
             >
               {BANNER_TEXT}
             </p>
-
-            {/* Button */}
             <button className="flex h-14 justify-center items-center gap-1 [background:var(--Background,#FFFEF8)] px-12 py-5 mt-8">
               <a
                 href="/history"
@@ -48,62 +40,67 @@ export default function Home() {
               </a>
             </button>
           </div>
-        </div>
+        </header>
 
-        {/* Overview Section */}
-        <TitleSection title="Giới thiệu" />
-        <div className="flex px-8 justify-between items-end self-stretch flex-wrap">
-          <span className="w-full md:w-[400px] text-[color:var(--Black,#1F1F1F)] text-left [leading-trim:both] [text-edge:cap] [font-family:Actor] text-[20px] font-normal leading-[28px] tracking-[-0.05em]">
-            {INTRO_TEXT}
-          </span>
-
-          <img
-            className="w-[400px] h-[300px]"
-            src="/home/monalisa.png"
-            alt="Mona Lisa"
-          />
-          <img
-            className="w-[400px] h-[600px]"
-            src="/home/pisa.png"
-            alt="Leaning Tower of Pisa"
-          />
-        </div>
-
-        {/*Explore Section */}
-        <TitleSection title="Khám Phá" />
-        <div className="flex-col mt-[40px]  flex px-8 justify-center">
-          <div className="flex gap-4 flex-wrap justify-center items-center">
-            <Link
-              href="/explore"
-              className="text-[color:var(--Black,#1F1F1F)] [leading-trim:both] [text-edge:cap] [font-family:Actor] text-base font-normal leading-[26px] uppercase"
-            >
-              Nhan vat tieu bieu
-            </Link>
-            <Link
-              href="/"
-              className="text-[color:var(--Black,#1F1F1F)] [leading-trim:both] [text-edge:cap] [font-family:Actor] text-base font-normal leading-[26px] uppercase"
-            >
-              Su kien lich su
-            </Link>
-            <Link
-              href="/"
-              className="text-[color:var(--Black,#1F1F1F)] [leading-trim:both] [text-edge:cap] [font-family:Actor] text-base font-normal leading-[26px] uppercase"
-            >
-              Tham quan online
-            </Link>
-          </div>
-
-          <div className="grid grid-cols-3">
-            {HistoricalFigures.map((figure,index)=>(
-              <ProfileCard
-                key={index}
-                name={figure.name}
-                content={figure.content}
-                image={figure.image}
+        <section>
+          <TitleSection title="Giới thiệu" />
+          <div className="flex px-8 justify-between items-end self-stretch flex-wrap">
+            <article className="w-full md:w-[400px] text-[color:var(--Black,#1F1F1F)] text-left [leading-trim:both] [text-edge:cap] [font-family:Actor] text-[20px] font-normal leading-[28px] tracking-[-0.05em]">
+              {INTRO_TEXT}
+            </article>
+            <figure>
+              <img
+                className="w-[400px] h-[300px]"
+                src="/home/monalisa.png"
+                alt="Mona Lisa"
               />
-            ))}
+            </figure>
+            <figure>
+              <img
+                className="w-[400px] h-[600px]"
+                src="/home/pisa.png"
+                alt="Leaning Tower of Pisa"
+              />
+            </figure>
+          </div>
+        </section>
+
+        <section>
+          <TitleSection title="Khám Phá" />
+          <div className="flex-col mt-[40px] flex px-8 justify-center">
+            <div className="flex gap-4 flex-wrap justify-center items-center">
+              <Link
+                href="/explore"
+                className="text-[color:var(--Black,#1F1F1F)] [leading-trim:both] [text-edge:cap] [font-family:Actor] text-base font-normal leading-[26px] uppercase"
+              >
+                Nhan vat tieu bieu
+              </Link>
+              <Link
+                href="/"
+                className="text-[color:var(--Black,#1F1F1F)] [leading-trim:both] [text-edge:cap] [font-family:Actor] text-base font-normal leading-[26px] uppercase"
+              >
+                Su kien lich su
+              </Link>
+              <Link
+                href="/"
+                className="text-[color:var(--Black,#1F1F1F)] [leading-trim:both] [text-edge:cap] [font-family:Actor] text-base font-normal leading-[26px] uppercase"
+              >
+                Tham quan online
+              </Link>
             </div>
-        </div>
+
+            <div className="grid grid-cols-3">
+              {HistoricalFigures.map((figure, index) => (
+                <ProfileCard
+                  key={index}
+                  name={figure.name}
+                  content={figure.content}
+                  image={figure.image}
+                />
+              ))}
+            </div>
+          </div>
+        </section>
       </main>
     </div>
   );
--- historicalsite/src/components/section/TitleSection.tsx
+++ historicalsite/src/components/section/TitleSection.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import React from "react";
 
 export default function TitleSection({ title }: { title: string }) {
@@ -5,16 +6,16 @@ export default function TitleSection({ title }: { title: string }) {
     <div className=" px-8 flex justify-between items-center self-stretch h-[200px] ">
       {/* Left Section */}
       <div className=" flex justify-center items-center  w-[446px] h-[170px] shrink-0 border border-[color:var(--Black,#1F1F1F)] rounded-full border-solid">
-        <p className="text-[color:var(--Black,#1F1F1F)] text-center [leading-trim:both] [text-edge:cap] [font-family:Actor] text-[64px] font-normal leading-[44px] tracking-[-3.2px] uppercase">
+        <h1 className="text-[color:var(--Black,#1F1F1F)] text-center [leading-trim:both] [text-edge:cap] [font-family:Actor] text-[64px] font-normal leading-[44px] tracking-[-3.2px] uppercase">
           {title}
-        </p>
+        </h1>
       </div>
 
       {/* Right Section */}
       <div className="flex justify-center items-center w-[200px] h-[50px] shrink-0  rounded-full bg-transparent cursor-pointer">
-        <a className="text-[color:var(--Black,#1F1F1F)] text-center [font-family:Actor] text-[20px] font-normal leading-[24px] tracking-[-1px] uppercase">
+        <Link href="/#" className="text-[color:var(--Black,#1F1F1F)] text-center [font-family:Actor] text-[20px] font-normal leading-[24px] tracking-[-1px] uppercase">
           Tìm Hiểu Thêm
-        </a>
+        </Link>
       </div>
     </div>
   );
