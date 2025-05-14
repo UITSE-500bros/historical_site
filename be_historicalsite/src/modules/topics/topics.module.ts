dc137de11dcdcfc6f63055faa6ba63a798e5be32
Đoàn Phương Nam <120766900+DPhngNam@users.noreply.github.com>
2025-05-14 15:38:27 +0700

    fix: error in jwtguard

--- be_historicalsite/src/modules/topics/topics.module.ts
+++ be_historicalsite/src/modules/topics/topics.module.ts
@@ -2,8 +2,10 @@ import { Module } from '@nestjs/common';
 import { TopicsService } from './topics.service';
 import { TopicsController } from './topics.controller';
 import { PrismaService } from '../../prisma/prisma.service';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
+  imports: [AuthModule],
   controllers: [TopicsController],
   providers: [TopicsService, PrismaService],
 })
