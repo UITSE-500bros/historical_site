f32d1b020cc43d8adeac665ed7e182a2eb1ed01c
ThnhHuynh04 <22521406@gm.uit.edu.vn>
2025-05-24 21:47:57 +0700

    feat: admin post page

--- historicalsite/src/app/admin/posts/page.tsx
+++ historicalsite/src/app/admin/posts/page.tsx
@@ -1,9 +1,88 @@
-import React from 'react'
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
+import { Pagination } from "@/components/ui/pagination";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import { MyPagination } from "@/src/components/section/MyPagination";
+import React from "react";
 
 export default function page() {
+  const posts = [
+    {
+      id: 1,
+      title: "Post 1",
+      category: "Event",
+      date: "2023-10-01",
+    },
+    {
+      id: 2,
+      title: "Post 2",
+      category: "News",
+      date: "2023-10-02",
+    },
+    {
+      id: 3,
+      title: "Post 3",
+      category: "Update",
+      date: "2023-10-03",
+    },
+    {
+      id: 4,
+      title: "Post 4",
+      category: "Announcement",
+      date: "2023-10-04",
+    },
+    {
+      id: 5,
+      title: "Post 5",
+      category: "Event",
+      date: "2023-10-05",
+    },
+  ];
   return (
-    <div>
-      
-    </div>
-  )
+    <Card className="w-full max-w-6xl mx-auto h-full flex items-center justify-start min-h-screen">
+      <CardHeader className="flex w-full justify-between items-center">
+        <CardTitle className="text-2xl font-semibold">Posts</CardTitle>
+        <Button className="p-4  "> New post</Button>
+      </CardHeader>
+      <CardContent className="w-full overflow-x-auto">
+        <Table className="w-full">
+          <TableHeader>
+            <TableRow>
+              <TableHead className="text-left w-12">Id</TableHead>
+              <TableHead className="w-64 text-left">Title</TableHead>
+              <TableHead className="w-48 text-left">Category</TableHead>
+              <TableHead className="w-40 text-left">Date</TableHead>
+              <TableHead className="w-48 text-left">Actions</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {posts.map((post) => (
+              <TableRow key={post.id}>
+                <TableCell className="w-12">{post.id}</TableCell>
+                <TableCell className="w-64">{post.title}</TableCell>
+                <TableCell className="w-48">{post.category}</TableCell>
+                <TableCell className="w-40">{post.date}</TableCell>
+                <TableCell className="w-48 flex gap-2">
+                  <Button className="p-2">Edit</Button>
+                  <Button className="p-2 ml-2" variant="destructive">
+                    Delete
+                  </Button>
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </CardContent>
+      <CardFooter>
+        <MyPagination />
+      </CardFooter>
+    </Card>
+  );
 }
